4256439ad4973f2bff8c0e2d31d7edbb18974a6f
Batuhan Atasoy <atasoybatuhan@outlook.com>
2024-01-15 16:26:59 +0300

    Developed encryption modules.

--- trial.py
+++ trial.py
@@ -1,1 +1,16 @@
-print("Hello!...This is an embedded bytecode works in Python.")
+from cryptography.fernet import Fernet
+
+# Generate the Keys
+key=Fernet.generate_key()
+cipher_suite=Fernet(key)
+
+# Text that will be encrypted
+original_text = "Merhaba, bu metin şifrelenecek."
+cipher_text = cipher_suite.encrypt(original_text.encode())
+
+print(f"Original Text: {original_text}")
+print(f"Encrypted Text: {cipher_text}")
+
+# Decoding the Encrypted Text
+decrypted_text = cipher_suite.decrypt(cipher_text).decode()
+print(f"Decoded Text: {decrypted_text}")
